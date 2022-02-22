6e4681b518fa361a33b9086854cbbd604d0a0ebd
Joseph Moreno <44370115+josephmoresena@users.noreply.github.com>
2022-02-22 07:43:14 -0500

    LoadLibrary
    JNI_OnLoad method

--- ExportedMethods.cs
+++ ExportedMethods.cs
@@ -12,11 +12,24 @@ namespace HelloJniLib
 #pragma warning disable IDE0060
     public static class ExportedMethods
     {
-        private static readonly Boolean DisabledReflection = !typeof(String).ToString().Contains(nameof(String));
+        private static readonly Boolean disabledReflection = !typeof(String).ToString().Contains(nameof(String));
+
+        private static DateTime? load = default;
+        private static Int32 count = 0;
+
+        [UnmanagedCallersOnly(EntryPoint = "JNI_OnLoad")]
+        public static Int32 LoadLibrary(JavaVMRef vm, IntPtr unknown)
+        {
+            load = DateTime.Now;
+            return 0x00010006; //JNI_VERSION_1_6
+        }
 
         [UnmanagedCallersOnly(EntryPoint = "Java_com_example_hellojni_HelloJni_stringFromJNI")]
         internal static JStringLocalRef Hello(JEnvRef jEnv, JObjectLocalRef jObj)
         {
+            DateTime call = DateTime.Now;
+            count++;
+
             JEnvValue value = jEnv.Environment;
             ref JNativeInterface jInterface = ref value.Functions;
 
@@ -25,12 +38,16 @@ namespace HelloJniLib
 
             String result =
                 "Hello from JNI! Compiled with NativeAOT." + Environment.NewLine
-                + GetRuntimeInformation();
+                + GetRuntimeInformation(call);
             return newString(jEnv, result.AsSpan().AsIntPtr(), result.Length);
         }
 
-        private static String GetRuntimeInformation()
-            => $"Number of Cores: {Environment.ProcessorCount}" + Environment.NewLine
+        private static String GetRuntimeInformation(DateTime call)
+            => $"Load: {load.GetString()}" + Environment.NewLine
+            + $"Call: {call.GetString()}" + Environment.NewLine
+            + $"Count: {count}"
+            + Environment.NewLine + Environment.NewLine
+            + $"Number of Cores: {Environment.ProcessorCount}" + Environment.NewLine
             + $"OS: {RuntimeInformation.OSDescription}" + Environment.NewLine
             + $"OS Arch: {RuntimeInformation.OSArchitecture.GetName()}" + Environment.NewLine
             + $"OS Version: {Environment.OSVersion}" + Environment.NewLine
@@ -38,20 +55,19 @@ namespace HelloJniLib
             + $"User: {Environment.UserName}" + Environment.NewLine
             + $"System Path: {Environment.SystemDirectory}" + Environment.NewLine
             + $"Current Path: {Environment.CurrentDirectory}" + Environment.NewLine
-            + $"Process Arch: {RuntimeInformation.ProcessArchitecture.GetName()}" + Environment.NewLine
-            + GetRuntimeReflectionInformation()
-            //+ $"Framework Version: {Environment.Version}" + Environment.NewLine //Error
-            + "";
-
-        private static String GetRuntimeReflectionInformation()
+            + $"Process Arch: {RuntimeInformation.ProcessArchitecture.GetName()}"
+            + Environment.NewLine + Environment.NewLine
+            + GetRuntimeInformation();
+        private static String GetRuntimeInformation()
+            => !disabledReflection ? GetRuntimeReflectionInformation() : "REFLECTION DISABLED";
+        private static string GetRuntimeReflectionInformation()
         {
-            if (!DisabledReflection)
-                return $"Runtime Name: {RuntimeInformation.FrameworkDescription}" + Environment.NewLine
-                    + $"Runtime Path: {RuntimeEnvironment.GetRuntimeDirectory()}" + Environment.NewLine
-                    + $"Runtime Version: {RuntimeEnvironment.GetSystemVersion()}" + Environment.NewLine;
-            return String.Empty;
+            return $"Framework Version: {Environment.Version}" + Environment.NewLine
+                + $"Runtime Name: {RuntimeInformation.FrameworkDescription}" + Environment.NewLine
+                + $"Runtime Path: {RuntimeEnvironment.GetRuntimeDirectory()}" + Environment.NewLine
+                + $"Runtime Version: {RuntimeEnvironment.GetSystemVersion()}" + Environment.NewLine
+                + Environment.NewLine;
         }
-
         private static String GetName(this Architecture architecture)
             => architecture switch
             {
@@ -63,6 +79,10 @@ namespace HelloJniLib
                 Architecture.X86 => nameof(Architecture.X86),
                 _ => architecture.ToString(),
             };
+        private static String GetString(this DateTime date)
+            => GetString((DateTime?)date);
+        private static String GetString(this DateTime? date)
+            => date != default ? date.Value.ToString("yyyy-MM-dd HH:mm:ss.fffffff") : "null";
     }
 #pragma warning restore IDE0060
 }
