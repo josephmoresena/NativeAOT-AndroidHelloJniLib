e69ee4c06f7dacd366f4931d4e3e298bb664c1dd
g <g@gmail.com>
2024-01-29 11:17:38 +0800

    build_blat

---
+++ build_bflat.bat
@@ -0,0 +1,4 @@
+#clean all temp file (obj/** ,bin/**) 
+
+bflat build -r BFlatSupport/Rxmxnx.PInvoke.Extensions.dll --no-stacktrace-data --no-globalization --no-exception-messages -Os --no-pie
+ --separate-symbols --os:linux --arch:arm64 --libc:bionic -o:libhello-jni.so
